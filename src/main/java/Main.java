723998156b0297bb2362a3e112fa1025c5a3b3bf
Artem1107 <1488artemartem@gmail.com>
2021-06-10 11:15:58 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class Main {
+
+
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Artem\\IdeaProjects\\autoLoginVk\\chromedriver_win32\\chromedriver.exe");
 
-
         WebDriver driver = new ChromeDriver();
+
         driver.get("https://vk.com");
         System.out.println("Успешно открыт сайт");
 
@@ -18,7 +20,7 @@ public class Main {
         System.out.println("Элемент найден");
         log.click();
         System.out.println("Поле выбрано");
-        log.sendKeys("89805015734", Keys.ENTER, "Simbakot1",Keys.ENTER);
+        log.sendKeys("89537493689", Keys.ENTER, "qwerty12asd",Keys.ENTER);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         System.out.println("Авторизция успешна");
         WebElement message = driver.findElement(By.xpath("//*[@id=\"l_msg\"]/a/span[1]"));
@@ -27,15 +29,15 @@ public class Main {
         WebElement search = driver.findElement(By.cssSelector("#im_dialogs_search"));
         search.click();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        search.sendKeys("Кирилл Майер");
+        search.sendKeys("Артём Александрович");
         System.out.println("Ввели в строку поиска имя");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        WebElement kir = driver.findElement(By.cssSelector("#im_dialogs > li.nim-dialog.nim-conversation-search-row._im_dialog._im_dialog_381910930.nim-csr_classic > div.nim-dialog--content > div > div.nim-dialog--name > span.nim-dialog--name-w > span"));
-        kir.click();
-        System.out.println("Открыт диалог с Кириллом");
+        WebElement art = driver.findElement(By.xpath("//span[text()= 'Артём Александрович']"));
+        art.click();
+        System.out.println("Открыт диалог");
         WebElement inputMessage = driver.findElement(By.xpath("//div[@class='im_editable im-chat-input--text _im_text']"));
         inputMessage.click();
-        inputMessage.sendKeys("Братишка я тебе автотесты принёс", Keys.ENTER);
+        inputMessage.sendKeys("Автосообщение", Keys.ENTER);
         System.out.println("Отправлено сообщение");
 
     }
